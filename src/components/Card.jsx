04e71677e9603870d11915566a1c4b5b1f38a98d
Euler496 <leonhardeuler496@gmail.com>
2023-09-11 10:00:30 +0300

    version 1.0.0

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card () {
+  return (
+    <div className="card">
+      <img src="./src/assets/image-qr-code.png"/>
+      <h2>Improve your front-end skills by building projects</h2>
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+  );
+}
+
+export default Card;
